e8edc7cc962bf37c5a1eb59fb3180f8558ff246d
Boning Li <Boning.li@outllok.com>
2017-11-15 01:01:58 +0100

    合并了前端vue视图，添加了htaccess文件以修复图标显示问题

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,16 +7,23 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+// import "bootstrap-sass";
+import App from './App.vue';
 
+Vue.config.productionTip = false;
+Vue.use(iView);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    template: '<App/>',
+    components: { App },
 });
