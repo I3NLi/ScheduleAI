7788989dda38d30f4e3bc4032bbcdd72b5bcca1d
Boning Li <344270545@qq.com>
2018-05-19 20:30:12 +0200

    完善了把他、

--- bin/installation.bat
+++ bin/installation.bat
@@ -7,3 +7,9 @@ docker-compose up -d apache2 mongo
 docker run -d -p 27017:27017 -v ./data://data/db --network laradock_backend --name laradock_mongo_1 mongo
 call docker-compose exec workspace composer install
 call docker-compose exec workspace php artisan migrate --seed
+call stop
+echo The installation is complete and continues to start or exit directly
+pause
+start docker-compose exec workspace bash
+cd ..
+start npm run watch
--- bin/run-dev.bat
+++ bin/run-dev.bat
@@ -2,6 +2,8 @@ cd ../laradock
 docker-compose up -d apache2 mongo
 docker container rm laradock_mongo_1
 docker run -d -p 27017:27017 -v ./data://data/db --network laradock_backend --name laradock_mongo_1 mongo
+echo  PrivateSecretary Started,now you can open http://localhost to use it
 start docker-compose exec workspace bash
 cd ..
 start npm run watch
+pause
