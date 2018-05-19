ca8f735a174f84ea49e24c1555bfdcefa1c87e01
Boning Li <344270545@qq.com>
2018-05-19 17:00:25 +0200

    加入了cross-env模块

--- bin/run-dev.bat
+++ bin/run-dev.bat
@@ -2,5 +2,6 @@ cd ../laradock
 docker-compose up -d apache2 mongo
 docker container rm laradock_mongo_1
 docker run -d -p 27017:27017 -v ./data://data/db --network laradock_backend --name laradock_mongo_1 mongo
+start docker-compose exec workspace bash
 cd ..
-npm run watch
+start npm run watch
