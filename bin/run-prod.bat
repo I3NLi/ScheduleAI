8f3fc998b3f400e72a18396cd75c226efd6b4265
Boning Li <344270545@qq.com>
2018-05-24 16:37:23 +0200

    追踪了bin目录中新的文件

---
+++ bin/run-prod.bat
@@ -0,0 +1,6 @@
+cd ../laradock
+docker-compose up -d apache2 mongo
+docker container rm laradock_mongo_1
+docker run -d -p 27017:27017 -v ./data://data/db --network laradock_backend --name laradock_mongo_1 mongo
+echo  PrivateSecretary Started,now you can open http://localhost to use it
+pause
---
+++ bin/stop.bat
@@ -0,0 +1,6 @@
+call docker container kill laradock_apache2_1
+call docker container kill laradock_php-fpm_1
+call docker container kill laradock_workspace_1
+call docker container kill laradock_mongo_1
+
+pause
