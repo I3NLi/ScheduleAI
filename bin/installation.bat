23e4ffe940708534c57b9b671e1a5b0223af969f
Boning Li <Boning.li@outlook.com>
2018-06-04 00:22:01 +0200

    修复了一些bug

--- bin/installation.bat
+++ bin/installation.bat
@@ -1,12 +1,14 @@
+call stop
 cd ..
 copy .env.example .env
 copy laradock.env laradock\.env
 call npm install --no-bin-links
 cd laradock
-docker-compose up -d apache2 mongo
+docker-compose up -d apache2
 docker run -d -p 27017:27017 -v ./data://data/db --network laradock_backend --name laradock_mongo_1 mongo
 call docker-compose exec workspace composer install
 call docker-compose exec workspace php artisan migrate --seed
+cd ../bin
 call stop
 echo The installation is complete and continues to start or exit directly
 pause
