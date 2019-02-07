e5d59d1f0ec2fce72f0b96a31fa09d94299e2f09
Boning Li <344270545@qq.com>
2019-02-08 00:17:36 +0100

    x

---
+++ resources/assets/application/mixin/onlineActivityController.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  methods: {
+    //加载所有活动
+    loadActivities(){
+
+
+    }
+    //创建活动
+    createActivity(){
+
+    }
+    //更新所有活动
+    updateActivities(){
+       this.$root.updateActivities();
+     }
+    //更新单一活动
+    updateActivity(activity){
+      this.$root.updateActivities();
+    }
+    //删除活动
+    deleteActivity(){
+       this.$root.updateActivities();
+     }
+  },
+  computed:{
+
+  },
+  components: {
+
+  },
+  watch:{
+
+  }
+};
+</script>
